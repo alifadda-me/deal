5b380f4d210cdc5b81c797825de7e734447987a8
silvertechguy <silvertechguy@gmail.com>
2024-05-23 17:55:34 +0300

    Remove un-used files

--- e2e_tests/setup/docker.mongo.d.ts
+++
@@ -1,4 +0,0 @@
-import { StartedTestContainer } from "testcontainers";
-export declare const MONGO_PORT = 27017;
-export declare function startContainer(): Promise<StartedTestContainer>;
-export declare function getMongoUriWithRandomizedDatabaseName(): string;
